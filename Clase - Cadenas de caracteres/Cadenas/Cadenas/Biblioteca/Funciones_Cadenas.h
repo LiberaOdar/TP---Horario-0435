cbdcd8601952b86d6eb4b70365a7a070165e5416
Libera <a20243954@pucp.edu.pe>
2026-05-28 18:44:41 -0500

    Add files via upload

---
+++ Clase - Cadenas de caracteres/Cadenas/Cadenas/Biblioteca/Funciones_Cadenas.h
@@ -0,0 +1,14 @@
+//
+// Created by cueva.r on 27/05/2026.
+//
+
+#ifndef CADENAS_FUNCIONES_CADENAS_H
+#define CADENAS_FUNCIONES_CADENAS_H
+
+void concatenar(char *destino, const char *fuente);
+int comparar(const char *cadena1,const char *cadena2);
+void copiar(char *destino, const char *fuente);
+int longitud(char*cadena);
+
+
+#endif //CADENAS_FUNCIONES_CADENAS_H
